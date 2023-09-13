195c6c14f37fdf6dd0611cd993c4848340646b65
mayanksrivastava6 <mayanksrivastava61103@gmail.com>
2023-09-14 00:23:04 +0530

    gg

--- routers/home.js
+++ routers/home.js
@@ -4,14 +4,7 @@ const Router = express.Router();
 const querystring = require("querystring");
 const axios = require("axios");
 
-Router.get("/", async (req, res) => {
-  try {
-    return res.status(200).send("Welcome to Home Screen");
-  } catch (error) {
-    return res.status(400).send(err.message);
-  }
-});
-const app = express();
+
 const verifyCaptcha = async (req, res, next) => {
   const { "g-recaptcha-response": token } = req.body;
   const secretKey = "6Le_np0mAAAAANvpwFAN6nd9PBub3_3jrWRLbsrY";
@@ -84,79 +77,3 @@ Router.post("/register", async (req, res) => {
 });
 
 module.exports = Router;
-// // Your registration endpoint
-// app.post('/register', async (req, res) => {
-//   const { recaptchaToken, ...otherData } = req.body;
-//   const secretKey="6Le_np0mAAAAANvpwFAN6nd9PBub3_3jrWRLbsrY"
-//   try {
-//     // Verify reCAPTCHA on the server
-//     const captchaResponse = await axios.post(
-//       'https://www.google.com/recaptcha/api/siteverify',
-//       `secret=${secretKey}&response=${recaptchaToken}`,
-//       {
-//         headers: {
-//           'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//       }
-//     );
-
-//     const captchaData = captchaResponse.data;
-
-//     if (captchaData.success) {
-//       try {
-//         const {
-//           username,
-//           email,
-//           studentNumber,
-//           phone,
-//           hackerRankUsername,
-//           UnstopUsername,
-//           section,
-//           branch,
-//           year,
-//           gender,
-//           residence,
-//         } = req.body;
-    
-//         const useremail = await homeSchema.findOne({
-//           email: email,
-//         });
-//         if (useremail) {
-//           console.log("User Already Exists");
-//           res.json({ success: false, msg: "User already exist" });
-//         } else {
-//           try {
-//             const userData = new homeSchema({
-//               username,
-//               email,
-//               studentNumber,
-//               phone,
-//               hackerRankUsername,
-//               UnstopUsername,
-//               section,
-//               branch,
-//               year,
-//               gender,
-//               residence,
-//             });
-//             await userData.save();
-//             res.json({ success: true, msg: "Registered Successfully" });
-//           } catch (err) {
-//             console.log(err);
-//             res.json({ succes: false });
-//           }
-//         }
-//       } catch (error) {
-//         console.log(error);
-//       }
-//       res.status(200).json({ message: 'Registration successful' });
-//     } else {
-//       // reCAPTCHA verification failed
-//       res.status(400).json({ error: 'reCAPTCHA verification failed' });
-//     }
-//   } catch (error) {
-//     // Handle any errors
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
